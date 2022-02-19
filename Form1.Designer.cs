5b1a8cee06f00d839ac9c78fb64cae64dffc9f21
Yusuf Kaan M <Yusuf Kaan M@DESKTOP-9K0M5IC>
2022-02-19 14:29:25 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Troll
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.timer10 = new System.Windows.Forms.Timer(this.components);
+            this.timer11 = new System.Windows.Forms.Timer(this.components);
+            this.timer12 = new System.Windows.Forms.Timer(this.components);
+            this.timer13 = new System.Windows.Forms.Timer(this.components);
+            this.timer14 = new System.Windows.Forms.Timer(this.components);
+            this.timer15 = new System.Windows.Forms.Timer(this.components);
+            this.timer16 = new System.Windows.Forms.Timer(this.components);
+            this.timer17 = new System.Windows.Forms.Timer(this.components);
+            this.timer18 = new System.Windows.Forms.Timer(this.components);
+            this.timer19 = new System.Windows.Forms.Timer(this.components);
+            this.timer20 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 1;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Enabled = true;
+            this.timer5.Interval = 1;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Enabled = true;
+            this.timer6.Interval = 1;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Enabled = true;
+            this.timer7.Interval = 1;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Enabled = true;
+            this.timer8.Interval = 1;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // timer9
+            // 
+            this.timer9.Enabled = true;
+            this.timer9.Interval = 1;
+            this.timer9.Tick += new System.EventHandler(this.timer9_Tick);
+            // 
+            // timer10
+            // 
+            this.timer10.Enabled = true;
+            this.timer10.Interval = 1;
+            this.timer10.Tick += new System.EventHandler(this.timer10_Tick);
+            // 
+            // timer11
+            // 
+            this.timer11.Enabled = true;
+            this.timer11.Interval = 1;
+            this.timer11.Tick += new System.EventHandler(this.timer11_Tick);
+            // 
+            // timer12
+            // 
+            this.timer12.Enabled = true;
+            this.timer12.Interval = 1;
+            this.timer12.Tick += new System.EventHandler(this.timer12_Tick);
+            // 
+            // timer13
+            // 
+            this.timer13.Enabled = true;
+            this.timer13.Interval = 1;
+            this.timer13.Tick += new System.EventHandler(this.timer13_Tick);
+            // 
+            // timer14
+            // 
+            this.timer14.Enabled = true;
+            this.timer14.Interval = 1;
+            this.timer14.Tick += new System.EventHandler(this.timer14_Tick);
+            // 
+            // timer15
+            // 
+            this.timer15.Enabled = true;
+            this.timer15.Interval = 1;
+            this.timer15.Tick += new System.EventHandler(this.timer15_Tick);
+            // 
+            // timer16
+            // 
+            this.timer16.Enabled = true;
+            this.timer16.Interval = 1;
+            this.timer16.Tick += new System.EventHandler(this.timer16_Tick);
+            // 
+            // timer17
+            // 
+            this.timer17.Enabled = true;
+            this.timer17.Interval = 1;
+            this.timer17.Tick += new System.EventHandler(this.timer17_Tick);
+            // 
+            // timer18
+            // 
+            this.timer18.Enabled = true;
+            this.timer18.Interval = 1;
+            this.timer18.Tick += new System.EventHandler(this.timer18_Tick);
+            // 
+            // timer19
+            // 
+            this.timer19.Enabled = true;
+            this.timer19.Interval = 1;
+            this.timer19.Tick += new System.EventHandler(this.timer19_Tick);
+            // 
+            // timer20
+            // 
+            this.timer20.Enabled = true;
+            this.timer20.Interval = 1;
+            this.timer20.Tick += new System.EventHandler(this.timer20_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 0);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+        private System.Windows.Forms.Timer timer10;
+        private System.Windows.Forms.Timer timer11;
+        private System.Windows.Forms.Timer timer12;
+        private System.Windows.Forms.Timer timer13;
+        private System.Windows.Forms.Timer timer14;
+        private System.Windows.Forms.Timer timer15;
+        private System.Windows.Forms.Timer timer16;
+        private System.Windows.Forms.Timer timer17;
+        private System.Windows.Forms.Timer timer18;
+        private System.Windows.Forms.Timer timer19;
+        private System.Windows.Forms.Timer timer20;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Troll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            MessageBox.Show("TROLL","TROLL",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            timer4.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            timer5.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            timer6.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            timer7.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            timer8.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer9_Tick(object sender, EventArgs e)
+        {
+            timer9.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer10_Tick(object sender, EventArgs e)
+        {
+            timer10.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer11_Tick(object sender, EventArgs e)
+        {
+            timer11.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer12_Tick(object sender, EventArgs e)
+        {
+            timer12.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer13_Tick(object sender, EventArgs e)
+        {
+            timer13.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer14_Tick(object sender, EventArgs e)
+        {
+            timer14.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer15_Tick(object sender, EventArgs e)
+        {
+            timer15.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer16_Tick(object sender, EventArgs e)
+        {
+            timer16.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer17_Tick(object sender, EventArgs e)
+        {
+            timer17.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer18_Tick(object sender, EventArgs e)
+        {
+            timer18.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer19_Tick(object sender, EventArgs e)
+        {
+            timer19.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void timer20_Tick(object sender, EventArgs e)
+        {
+            timer20.Interval = 1;
+            MessageBox.Show("TROLL", "TROLL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
